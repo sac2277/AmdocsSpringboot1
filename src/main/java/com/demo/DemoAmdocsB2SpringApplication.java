4dabcdcce3cbfa2337d51fdbb975441c88790619
sac2277 <sachinnlohar@gmail.com>
2023-08-14 14:55:53 +0530

    first commit

---
+++ src/main/java/com/demo/DemoAmdocsB2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAmdocsB2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAmdocsB2SpringApplication.class, args);
+	}
+
+}
